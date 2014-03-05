10506dafa4fa3e45112a387de96a413c2e76befb
Akira Kusumoto <akirakusumo10@gmail.com>
2014-03-05 17:03:20 +0900

    Use selenium-chrome brew install chromedriver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,15 @@ require 'capybara/rspec'
 Dir.glob('spec/steps/**/*steps.rb') { |f| load f, true }
 Dir.glob('spec/support/**/*.rb')    { |f| require "./#{f}" }
 
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
 Capybara.configure do |config|
   config.run_server        = false
-  config.default_driver    = :selenium
-  config.javascript_driver = :selenium
+  config.default_driver    = :selenium_chrome
+  config.javascript_driver = :selenium_chrome
+
   config.app_host          = 'http://localhost:3000'
 end
 
