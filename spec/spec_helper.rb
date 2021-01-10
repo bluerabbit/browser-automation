d538b5fd7ff253775e10d3191cece74b3c56e037
Akira Kusumoto <akirakusumo10@gmail.com>
2021-01-10 20:59:37 +0900

    :recycle: Refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,17 +9,7 @@ Dir.glob('spec/steps/**/*steps.rb') { |f| load f, true }
 Dir.glob('spec/support/**/*.rb')    { |f| require "./#{f}" }
 
 Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('disable-notifications')
-  options.add_argument('disable-translate')
-  options.add_argument('disable-extensions')
-  options.add_argument('disable-infobars')
-  options.add_argument('window-size=1280,960')
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
 Capybara.configure do |config|
